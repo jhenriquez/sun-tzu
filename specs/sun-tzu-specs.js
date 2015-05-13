a2036b3f6b1047283a6a460790b539a90310633f
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-05-12 22:20:08 -0400

    Added tests for #key

--- specs/sun-tzu-specs.js
+++ specs/sun-tzu-specs.js
@@ -22,7 +22,7 @@ describe('Sun-Tzu', function () {
       tzu = new Tzu(undefined);  
     });
     
-    it('#getCurrentUser - should match the fixed username', function (done) {
+    it('#getCurrentUser - should match the configured (on environment) username', function (done) {
   		 tzu.getCurrentUser()
   		   .then(function (user) {
   		      user.username.should.eql('some_user');
@@ -30,25 +30,31 @@ describe('Sun-Tzu', function () {
   		   }, function (e) {
   			 done(new Error('Unexpected Error: ' + e));
   		   });
-  	  });
+  	});
+    
+    it('#key - should match the configured (on environment)', function () {
+      tzu.key.should.eql(process.env.CW_ACCESS_KEY);
+    });
   });
   
   describe('Setup - Constructor', function () {
     before(function () {
       dotenv.load();
-      tzu = new Tzu({ username: 'constructor_user' });  
+      tzu = new Tzu({ username: 'constructor_user', access_key: 'constructor_access_key' });  
     });
     
-  it('#getCurrentUser - should match the fixed username', function (done) {
-    tzu.getCurrentUser()
-     .then(function (user) {
-        user.username.should.eql('constructor_user');
-      done();
-     }, function (e) {
-       done(new Error('Unexpected Error: ' + e));
-     });
+    it('#getCurrentUser - should match configured (by construction) username', function (done) {
+      tzu.getCurrentUser()
+       .then(function (user) {
+          user.username.should.eql('constructor_user');
+        done();
+       }, function (e) {
+         done(new Error('Unexpected Error: ' + e));
+       });
+    });
+    
+    it('#key - should match the configured (by constructor)', function () {
+      tzu.key.should.eql('constructor_access_key');
     });
   });
-  
-  
 });

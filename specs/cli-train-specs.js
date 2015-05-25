dfa33d08be73667c76dda65cd4ddcc59d627ceaa
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-05-25 00:31:33 -0400

    Added specs for fall back to configuration

--- specs/cli-train-specs.js
+++ specs/cli-train-specs.js
@@ -6,10 +6,22 @@ var dirtyChai = require('dirty-chai');
 
 describe('tain - Command-Line Interface', function () {
   var languageDir = path.join(process.cwd(),'javascript');
+  var configFile  = path.join(process.cwd(), cli.CONFIGURATION_FILENAME);
   
-  before(function () {
+  before(function (done) {
     chai.use(dirtyChai);
-    chai.should();  
+    chai.should();
+    
+    cli.init({
+      username: 'specs_user',
+      access_key: 'specs_access_key',
+      language: 'javascript'
+    })
+    .then(function () { done(); }, done);
+  });
+  
+  after(function (done) {
+    fs.unlink(configFile, function () { done(); });
   });
   
   afterEach(function () {
@@ -39,5 +51,41 @@ describe('tain - Command-Line Interface', function () {
     });
   });
   
-  it('should fall back to configuration (default language) if none is provided');	
+  it('should fall back to configuration (default language) if none is provided', function (done) {
+    fs.stat(languageDir, function (err, pathInfo) {
+      chai.expect(err).to.be.ok();
+      err.code.should.eql('ENOENT');
+      
+      cli.train()
+      .then(function () {
+        fs.stat(languageDir, function (err, pathInfo) {
+          chai.expect(err).to.not.be.ok();
+          chai.expect(pathInfo).to.be.ok();
+          pathInfo.isDirectory().should.be.ok();
+          done();
+        });
+      }, function (err) {
+        done(err);
+      });
+    });
+  });
+  
+  it('should warn if no language is provided and no default language is configured.', function (done) {
+    fs.stat(languageDir, function (err, pathInfo) {
+      chai.expect(err).to.be.ok();
+      err.code.should.eql('ENOENT');
+      
+      cli
+        .init({ username: 'specs_user', access_key: 'specs_access_key', }, { force: true })
+        .then(function () {
+          cli.train()
+          .then(function () {
+            done(new Error('Something is wrong. This interaction should have failed.'));
+          }, function (err) {
+            err.message.should.match(/language/);
+            done();
+          });
+        });
+    });
+  });
 });
